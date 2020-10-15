8aac74bb93fbd8680189683481679aaecb5d4ca8
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-10-16 00:14:01 +0300

    удаление обработчика ESC при переключении карточек по меткам

--- js/map.js
+++ js/map.js
@@ -1,92 +1,94 @@
 'use strict';
 (function () {
 
-  const filtersContainer = document.querySelector(`.map__filters-container`);
-  const map = document.querySelector(`.map`);
-  const pinsList = map.querySelector(`.map__pins`);
-  const pinsAmount = 5;
+    const filtersContainer = document.querySelector(`.map__filters-container`);
+    const map = document.querySelector(`.map`);
+    const pinsList = map.querySelector(`.map__pins`);
+    const pinsAmount = 5;
 
-  let getPinsBlock = function (array) {
-    let fragment = document.createDocumentFragment();
-    for (let i = 0; i < pinsAmount; i++) {
-      if (window.pin.renderPin(array[i])) {
-        fragment.append(window.pin.renderPin(array[i]));
-      }
-    }
-    return fragment;
-  };
+    let getPinsBlock = function (array) {
+        let fragment = document.createDocumentFragment();
+        for (let i = 0; i < pinsAmount; i++) {
+            if (window.pin.renderPin(array[i])) {
+                fragment.append(window.pin.renderPin(array[i]));
+            }
+        }
+        return fragment;
+    };
 
-  let appendNewCard = function (evt) {
-    let buttonPin = evt.target.closest(`.map__pin`);
-    let buttonPinMain = evt.target.closest(`.map__pin--main`);
+    let renderNewCard = null;
 
-
-    if (buttonPin && !buttonPinMain) {
-
-      let onCardEscPress = function (evtEsc) {
-        window.utils.isEscEvent(evtEsc, closeCard);
-      };
-
-      let closeCard = function () {
+    let closeCard = function () {
         renderNewCard.remove();
         document.removeEventListener(`keydown`, onCardEscPress);
-      };
+    };
+    
+    let onCardEscPress = function (evtEsc) {
+        console.log('*** ESC')
+        window.utils.isEscEvent(evtEsc, closeCard);
+    };
 
-      // Проверка, есть ли открытая карточка
-      let mapCard = map.querySelector(`.map__card`);
-      if (mapCard) {
-        mapCard.remove();
-      }
-
-
-      // Отрисовка новой карточки
-      let renderNewCard = window.card.renderCard(buttonPin.addObj);
-
-      filtersContainer.before(renderNewCard);
-
-      let buttonCloseCard = renderNewCard.querySelector(`.popup__close`);
-
-      buttonCloseCard.addEventListener(`click`, function () {
-        closeCard();
-      });
-
-      document.addEventListener(`keydown`, onCardEscPress);
-    }
-  };
+    let appendNewCard = function (evt) {
+        let buttonPin = evt.target.closest(`.map__pin`);
+        let buttonPinMain = evt.target.closest(`.map__pin--main`);
 
 
-  window.map = {
-    renderPins() {
-      pinsList.append(getPinsBlock(window.data.arrayData));
-    },
-    removePins() {
-      let pinsItem = pinsList.children;
+        if (buttonPin && !buttonPinMain) {
+            // Проверка, есть ли открытая карточка
+            let mapCard = map.querySelector(`.map__card`);
+            if (mapCard) {
+                mapCard.remove();
+            }
 
-      for (let i = pinsItem.length - 1; i >= 0; i--) {
-        if (pinsItem[i].classList.contains(`map__pin`) && !pinsItem[i].classList.contains(`map__pin--main`)) {
-          pinsItem[i].remove();
+
+            // Отрисовка новой карточки
+            renderNewCard = window.card.renderCard(buttonPin.addObj);
+
+            filtersContainer.before(renderNewCard);
+
+            let buttonCloseCard = renderNewCard.querySelector(`.popup__close`);
+
+            buttonCloseCard.addEventListener(`click`, function () {
+                closeCard();
+            });
+
+            document.addEventListener(`keydown`, onCardEscPress);
         }
-      }
-    },
-    renderCardOnClick() {
-      pinsList.addEventListener(`click`, function (evt) {
-        appendNewCard(evt);
-      });
-    },
-    renderCardOnEnter() {
-      map.addEventListener(`keydown`, function (evt) {
-        if (evt.key === `Enter`) {
-          evt.preventDefault();
-          appendNewCard(evt);
+    };
+
+
+    window.map = {
+        renderPins() {
+            pinsList.append(getPinsBlock(window.data.arrayData));
+        },
+        removePins() {
+            let pinsItem = pinsList.children;
+
+            for (let i = pinsItem.length - 1; i >= 0; i--) {
+                if (pinsItem[i].classList.contains(`map__pin`) && !pinsItem[i].classList.contains(`map__pin--main`)) {
+                    pinsItem[i].remove();
+                }
+            }
+        },
+        renderCardOnClick() {
+            pinsList.addEventListener(`click`, function (evt) {
+                appendNewCard(evt);
+            });
+        },
+        renderCardOnEnter() {
+            map.addEventListener(`keydown`, function (evt) {
+                if (evt.key === `Enter`) {
+                    evt.preventDefault();
+                    appendNewCard(evt);
+                }
+            });
+        },
+        removeCard() {
+            let card = map.querySelector(`.map__card`);
+            if (card) {
+                card.remove();
+            }
         }
-      });
-    },
-    removeCard() {
-      let card = map.querySelector(`.map__card`);
-      if (card) {
-        card.remove();
-      }
-    }
-  };
+    };
 
 })();
