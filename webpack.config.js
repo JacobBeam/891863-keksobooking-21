7ebdb6f17f36d4a5a641fa1305e72efb1d1e76af
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-10-21 14:42:10 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/utils.js",
+    "./js/backend.js",
+    "./js/data.js",
+    "./js/form.js",
+    "./js/pin.js",
+    "./js/card.js",
+    "./js/map.js",
+    "./js/main.js",
+    "./js/drag.js"
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+}
